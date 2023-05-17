f3b2312e3bc2f64444d52c1b3941847458a7e704
Li Wang <li@liwang.info>
2023-05-16 19:25:42 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 import setuptools
-import sys
-import os
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-def get_requirements(path):
-    with open(path, encoding="utf-8") as requirements:
-        return [requirement.strip() for requirement in requirements]
-
-
-install_requires = get_requirements(os.path.join(base_dir, "requirements.txt"))
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -34,5 +22,5 @@ setuptools.setup(
         "Topic :: Text Processing :: Linguistic",
     ),
     setup_requires=["cython", "numpy"],
-    install_requires=install_requires,
+    install_requires=["cython", "numpy", "nltk>=3.2"],
 )
