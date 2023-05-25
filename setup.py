5ddaf5533984752dd7b24011845ae58cbf5e4219
Li Wang <li@liwang.info>
2023-05-25 14:27:10 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 import setuptools
+import sys
+
+from Cython.Build import cythonize
+USE_CYTHON = True
+
+# try:
+#     from Cython.Build import cythonize
+#     USE_CYTHON = True
+# except ImportError:
+#     sys.exit("Could not import Cython, which is required to build extension modules.")
+
+try:
+    import numpy as np
+except ImportError:
+    sys.exit("Could not import numpy, which is required to build the extension modules.")
+
+extensions = cythonize("dc_parser/*.pyx")
+for ext_module in extensions:
+    ext_module.include_dirs.append(np.get_include())
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
     name="disfluency-constituency-parser",
-    version="0.0.7",
+    version="0.0.8",
     author="Li Wang",
     author_email="li@liwang.info",
     long_description=long_description,
@@ -13,6 +33,7 @@ setuptools.setup(
     url="https://github.com/liwangd/disfluency-constituency-parser",
     packages=setuptools.find_packages(),
     package_data={'': ['*.pyx']},
+    ext_modules = cythonize(extensions),
     classifiers=(
         'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
